b07ee46a41f62b4d0c9381e00a9469699e227178
victorhermon <victorsilva.silva6@gmail.com>
2022-03-19 15:09:55 -0300

    Inserção do url minha_consulta para passar as informações digitas no formulario, criação da view e criação do html minha_consulta para exibição

--- passagens/views.py
+++ passagens/views.py
@@ -7,3 +7,11 @@ def index(request):
         'form': PassagemForms()
     }
     return render(request, 'index.html', contexto)
+
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        contexto = {
+            'form': PassagemForms(request.POST)
+        }
+        return render(request, 'minha_consulta.html', contexto)
