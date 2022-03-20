0df91f2714afc1892601aef89c71825a6998df6e
victorhermon <victorsilva.silva6@gmail.com>
2022-03-19 21:42:22 -0300

    Inseridos novos campos no formularios com novos tipos de dados e exibir esses dados na consulta

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classe
 
 
 class PassagemForms(forms.Form):
@@ -7,3 +9,12 @@ class PassagemForms(forms.Form):
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today())
+    classe_viagem = forms.ChoiceField(label='Classe da Viagem', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações Extras',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='E-mail')
